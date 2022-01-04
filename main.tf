3d0117229555815b8612ea4de7f0839ad6ec1ce0
Carlo van Overbeek <carlovo@gmail.com>
2022-01-04 15:21:01 +0100

    create alias pages for articles too

--- main.tf
+++ main.tf
@@ -10,22 +10,27 @@ variable "topic" {
   type = string
 }
 
+locals {
+  selected_content_path = "content/${var.topic}/"
+  file_endings          = toset(["", "/", ".html"])
+}
+
 resource "aws_s3_bucket_object" "index" {
-  for_each = toset(["", "/", ".html"])
+  for_each = local.file_endings
 
   bucket       = var.bucket
   key          = "${var.topic}${each.key}"
   content_type = "text/html"
 
-  content = file("content/${var.topic}/index.html")
+  content = file("${local.selected_content_path}index.html")
 }
 
 resource "aws_s3_bucket_object" "articles" {
-  for_each = fileset(path.module, "content/${var.topic}/articles/html/*.html")
+  for_each = { for product in setproduct(fileset(path.module, "${local.selected_content_path}articles/html/*.html"), local.file_endings) : "${var.topic}/${split(".", split("/", product[0])[4])[0]}${product[1]}" => product[0] }
 
   bucket       = var.bucket
-  key          = "${var.topic}/${split("/", each.key)[4]}"
+  key          = each.key
   content_type = "text/html"
 
-  content = file(each.key)
+  content = file(each.value)
 }
