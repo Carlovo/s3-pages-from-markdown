7466d701bbccabee8c7176af17ce55f6d1b4da91
Carlo van Overbeek <carlovo@gmail.com>
2022-05-27 22:17:38 +0200

    nest article subfolders

--- render.py
+++ render.py
@@ -11,18 +11,12 @@ bucket = sys.argv[2]
 content_dir = 'content'
 
 source_topic_dir = f'{content_dir}/source/{topic}'
-article_md_dir = source_topic_dir + '/articles'
 footer_file = source_topic_dir + '/appendices/footer.html'
 js_app_file = source_topic_dir + '/appendices/main.js'
 
 product_dir = content_dir + '/product'
-product_topic_dir = f'{product_dir}/{topic}-{bucket}/'
-article_html_dir = product_topic_dir + '/' + topic + '/'
-
-articles_filenames = sorted([
-    filename.split('.')[0]
-    for filename in os.listdir(article_md_dir)
-])
+product_topic_dir = f'{product_dir}/{topic}-{bucket}'
+article_html_dir = product_topic_dir + '/' + topic
 
 with open('html_template.jinja', 'r') as input_file:
     template_str = input_file.read()
@@ -36,9 +30,6 @@ if os.path.isdir(product_topic_dir):
     shutil.rmtree(product_topic_dir)
 
 os.mkdir(product_topic_dir)
-os.mkdir(article_html_dir)
-
-articles_info = {}
 
 if os.path.isfile(footer_file):
     with open(footer_file, 'r') as input_file:
@@ -52,35 +43,87 @@ if os.path.isfile(js_app_file):
 else:
     js_ref = ''
 
-for name in articles_filenames:
 
-    with open(f'{article_md_dir}/{name}.md', 'r', encoding='utf-8') as input_file:
-        text = input_file.read()
-
-    md = markdown.Markdown(extensions=['toc'])
-    body = md.convert(text) + footer_html
-
-    html_raw = template_j2.render(title=name, body=body)
-
-    root = xml.etree.ElementTree.fromstring(html_raw)
-
-    articles_info[name] = root.find('body/h1').text
-
-    html_pretty = template_j2.render(
-        title=articles_info[name],
-        body=body,
-        script=js_ref
+def convert_snake_case_to_title_case(text: str) -> str:
+    return ' '.join(
+        [substring.capitalize() for substring in text.split("_")]
     )
 
-    with open(f'{article_html_dir}/{name}.html', 'w', encoding='utf-8', errors='xmlcharrefreplace') as output_file:
-        output_file.write(html_pretty)
 
-index_body = f'<h1>{topic}</h1>\n<ul>\n' + ''.join(
-    [f'<li><a href=\"/{topic}/{filename}.html\">{articles_info[filename]}</a></li>\n'
-     for filename in articles_info.keys()]
-) + '</ul>'
+def create_html_link_list(path: str, text: str) -> str:
+    return f'<li><a href=\"{path}\">{text}</a></li>'
 
-index_html = template_j2.render(title=topic, body=index_body)
 
-with open(f'{product_topic_dir}/{topic}.html', 'w', encoding='utf-8') as output_file:
-    output_file.write(index_html)
+def create_html_link_list_from_dict(files_to_titles: dict) -> str:
+    return '<ul>\n' + '\n'.join(
+        [create_html_link_list(
+            file_to_title,
+            files_to_titles[file_to_title]
+        )
+            for file_to_title in files_to_titles.keys()]
+    ) + '\n</ul>\n'
+
+
+for dirpath, dirnames, filenames in os.walk(source_topic_dir + '/articles'):
+    articles_info = {}
+    online_path = '/'.join(dirpath.split('/')[4:])
+    # last argument is a hacky way to make all paths end the same way: with /
+    article_html_subdir = os.path.join(article_html_dir, online_path, '')
+
+    os.mkdir(article_html_subdir)
+    filenames.sort()
+
+    for filename in filenames:
+        with open(os.path.join(dirpath, filename), 'r', encoding='utf-8') as input_file:
+            text = input_file.read()
+
+        name = filename.split('.')[0]
+
+        md = markdown.Markdown(extensions=['toc'])
+        body = md.convert(text) + footer_html
+
+        html_raw = template_j2.render(title=name, body=body)
+        root = xml.etree.ElementTree.fromstring(html_raw)
+        article_title = root.find('body/h1').text
+
+        html_pretty = template_j2.render(
+            title=article_title,
+            body=body,
+            script=js_ref
+        )
+
+        html_filename = name + '.html'
+
+        with open(os.path.join(article_html_subdir, html_filename), 'w', encoding='utf-8', errors='xmlcharrefreplace') as output_file:
+            output_file.write(html_pretty)
+
+        articles_info[
+            '/' + os.path.join(topic, online_path, html_filename)
+        ] = article_title
+
+    parent_dir = article_html_subdir.split("/")[-2]
+    parent_title = convert_snake_case_to_title_case(parent_dir)
+
+    index_body = f'<h1>{parent_title}</h1>\n'
+
+    if dirnames:
+        dirnames.sort()
+        dirnames_info = {
+            f'/{os.path.join(topic, online_path, dirname)}.html': convert_snake_case_to_title_case(dirname)
+            for dirname in dirnames
+        }
+
+        index_body += '<h2>Topics</h2>\n' + \
+            create_html_link_list_from_dict(dirnames_info)
+
+    if filenames:
+        index_body += '<h2>Articles</h2>\n' + \
+            create_html_link_list_from_dict(articles_info)
+
+    index_html = template_j2.render(
+        title=parent_title,
+        body=index_body
+    )
+
+    with open(os.path.join(article_html_subdir, '..', f'{parent_dir}.html'), 'w', encoding='utf-8') as output_file:
+        output_file.write(index_html)
